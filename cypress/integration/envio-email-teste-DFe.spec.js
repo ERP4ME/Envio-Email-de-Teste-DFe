5c93e36956f7e515d63f0100d0bb9c5d116d72d4
ERP4ME <erp4me.automatizado@gmail.com>
2022-05-13 14:54:23 -0300

    refatorar

--- cypress/integration/envio-email-teste-DFe.spec.js
+++ cypress/integration/envio-email-teste-DFe.spec.js
@@ -4,6 +4,16 @@
 describe('Envio de Email teste', () => {
     beforeEach(() => {
         cy.login()
+        Cypress.on('uncaught:exception', (err, runnable, promise) => {
+            // when the exception originated from an unhandled promise
+            // rejection, the promise is provided as a third argument
+            // you can turn off failing the test in this case
+            if (promise) {
+              return false
+            }
+            // we still want to ensure there are no other unexpected
+            // errors, so we let them fail the test
+          })
         cy.intercept('GET', '/koopon-core-rest-api/empresa/configuracoes',
             { fixture: 'email-alterdata' }).as('dadosEmail')
 
